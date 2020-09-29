93af99c916089f419d9cf946126dbe897fc5a59c
jakejoue <jakejoue@live.com>
2020-09-29 17:44:05 +0800

    去除注释

--- src/geometry/sphere.js
+++ src/geometry/sphere.js
@@ -1,7 +1,6 @@
 // Primitives inspired by TDL http://code.google.com/p/webglsamples/,
 // copyright 2011 Google Inc. new BSD License
 // (http://www.opensource.org/licenses/bsd-license.php).
-/* eslint-disable max-statements, complexity */
 function tesselateSphere(props = {}) {
     const {
         nlat = 10,
