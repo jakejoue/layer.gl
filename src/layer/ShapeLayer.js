0b27c625e7a2cdf9e763de3d4fc50366cf380107
huangwei <jakejoue@live.com>
2020-11-09 09:23:01 +0800

    remove isShow

--- src/layer/ShapeLayer.js
+++ src/layer/ShapeLayer.js
@@ -23,7 +23,6 @@ export default class ShapeLayer extends Layer {
     constructor(options) {
         super(options);
 
-        this._isShow = true;
         options = this.getOptions();
         if (
             "windowAnimation" === options.style ||
@@ -181,109 +180,107 @@ export default class ShapeLayer extends Layer {
         const gl = transferOptions.gl,
             matrix = transferOptions.matrix;
 
-        if (this._isShow) {
-            const data = this.getData();
-            if (data && !(0 >= data.length)) {
-                const options = this.getOptions();
+        const data = this.getData();
+        if (data && !(0 >= data.length)) {
+            const options = this.getOptions();
 
-                const program = this.program;
-                program.use(gl);
+            const program = this.program;
+            program.use(gl);
 
-                if (!this.isUseTexture || this.texture) {
-                    gl.disable(gl.CULL_FACE);
+            if (!this.isUseTexture || this.texture) {
+                gl.disable(gl.CULL_FACE);
 
-                    if (options.blend) {
-                        gl.enable(gl.BLEND);
-                        gl.blendFunc(gl.ONE, gl.ONE);
-                        gl.blendEquation(gl.FUNC_ADD);
-                    }
+                if (options.blend) {
+                    gl.enable(gl.BLEND);
+                    gl.blendFunc(gl.ONE, gl.ONE);
+                    gl.blendEquation(gl.FUNC_ADD);
+                }
 
-                    if (0 === options.height) {
-                        gl.enable(gl.BLEND);
-                        gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
-                    }
+                if (0 === options.height) {
+                    gl.enable(gl.BLEND);
+                    gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+                }
 
-                    const style = LayerStyles[options.style] || 0;
-                    if ("gradual" === options.style) {
-                        gl.depthMask(false);
-                        gl.enable(gl.BLEND);
-                        gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
-                    } else {
-                        gl.depthMask(true);
-                    }
+                const style = LayerStyles[options.style] || 0;
+                if ("gradual" === options.style) {
+                    gl.depthMask(false);
+                    gl.enable(gl.BLEND);
+                    gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+                } else {
+                    gl.depthMask(true);
+                }
 
-                    // 顶部颜色
-                    const topColor = this.normizedColor(options.topColor);
+                // 顶部颜色
+                const topColor = this.normizedColor(options.topColor);
 
-                    // 波纹图层
-                    if (
-                        "ripple" === options.style &&
-                        options.rippleLayer &&
-                        options.rippleLayer.data &&
-                        options.rippleLayer.data[0]
-                    ) {
-                        const center = this.normizedPoint(
-                            options.rippleLayer.data[0].geometry.coordinates
-                        );
-                        program.setUniforms({
-                            u_ripple_center: [center[0], center[1], 0],
-                            u_radius:
-                                options.rippleLayer.options.size *
-                                options.rippleLayer.currentScale,
-                        });
-                    }
-                    // 光照
-                    let light_dir = [0, -1, 2];
-                    if (options.lightDir) {
-                        light_dir = [
-                            options.lightDir[0] || 0,
-                            options.lightDir[1] || 0,
-                            options.lightDir[2] || 0,
-                        ];
-                    }
-
-                    program.setUniforms(
-                        Object.assign(this.getCommonUniforms(transferOptions), {
-                            u_matrix: matrix,
-                            u_zoom_unit: this.normizedHeight(
-                                1,
-                                this.map.getCenter()
-                            ),
-                            style: style,
-
-                            // 纹理相关
-                            u_use_texture: this.isUseTexture,
-                            u_sampler: this.texture,
-
-                            // window样式下的整体的透明度
-                            alpha: parseFloat(options.opacity),
-                            // 顶部颜色，ripple模式下的使用
-                            top_color: topColor,
-
-                            // 时间相关
-                            time: new Date() - this.initializeTime,
-                            dataTime: new Date() - this.dataTime,
-                            riseTime: options.riseTime,
-                            // 光照相关
-                            u_use_lighting: options.useLight,
-                            u_side_light_dir: light_dir,
-                        })
+                // 波纹图层
+                if (
+                    "ripple" === options.style &&
+                    options.rippleLayer &&
+                    options.rippleLayer.data &&
+                    options.rippleLayer.data[0]
+                ) {
+                    const center = this.normizedPoint(
+                        options.rippleLayer.data[0].geometry.coordinates
                     );
-                    const dataMgrData = this.dataMgr.getData();
-                    if (dataMgrData.vertex.length > 0) {
-                        this.vertexArray.bind();
-                        this.indexBuffer.bind();
+                    program.setUniforms({
+                        u_ripple_center: [center[0], center[1], 0],
+                        u_radius:
+                            options.rippleLayer.options.size *
+                            options.rippleLayer.currentScale,
+                    });
+                }
+                // 光照
+                let light_dir = [0, -1, 2];
+                if (options.lightDir) {
+                    light_dir = [
+                        options.lightDir[0] || 0,
+                        options.lightDir[1] || 0,
+                        options.lightDir[2] || 0,
+                    ];
+                }
 
-                        gl.drawElements(
-                            gl.TRIANGLES,
-                            dataMgrData.index.length,
-                            gl.UNSIGNED_INT,
-                            0
-                        );
-                    }
+                program.setUniforms(
+                    Object.assign(this.getCommonUniforms(transferOptions), {
+                        u_matrix: matrix,
+                        u_zoom_unit: this.normizedHeight(
+                            1,
+                            this.map.getCenter()
+                        ),
+                        style: style,
+
+                        // 纹理相关
+                        u_use_texture: this.isUseTexture,
+                        u_sampler: this.texture,
+
+                        // window样式下的整体的透明度
+                        alpha: parseFloat(options.opacity),
+                        // 顶部颜色，ripple模式下的使用
+                        top_color: topColor,
+
+                        // 时间相关
+                        time: new Date() - this.initializeTime,
+                        dataTime: new Date() - this.dataTime,
+                        riseTime: options.riseTime,
+                        // 光照相关
+                        u_use_lighting: options.useLight,
+                        u_side_light_dir: light_dir,
+                    })
+                );
+                const dataMgrData = this.dataMgr.getData();
+                if (dataMgrData.vertex.length > 0) {
+                    this.vertexArray.bind();
+                    this.indexBuffer.bind();
+
+                    gl.drawElements(
+                        gl.TRIANGLES,
+                        dataMgrData.index.length,
+                        gl.UNSIGNED_INT,
+                        0
+                    );
                 }
             }
-        } else this.webglLayer.clear();
+        }
     }
 
     loadTexture(callBack) {
