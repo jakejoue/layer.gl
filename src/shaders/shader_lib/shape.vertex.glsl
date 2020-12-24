f6f283cabd278b47455c1145a55950dec025763f
jakejoue <jakejoue@live.com>
2020-12-24 11:42:30 +0800

    shader format

--- src/shaders/shader_lib/shape.vertex.glsl
+++ src/shaders/shader_lib/shape.vertex.glsl
@@ -52,9 +52,9 @@ void main() {
     vec4 icolor = a_color;
 
     #if defined(PICK)
-        if (mapvIsPicked()) {
-            icolor = uSelectedColor;
-        }
+    if (mapvIsPicked()) {
+        icolor = uSelectedColor;
+    }
     #endif
     
     // 如果使用光照
